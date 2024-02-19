511044786b85ca18ea217c16d83134f0f133d4d7
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-18 21:52:46 -0300

    feat: Config e resource projetct

--- LazyIterationTest/Program.cs
+++ LazyIterationTest/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Configuration;
+
+namespace LazyIterationTest
+{
+    class Program
+    {
+        public static IConfiguration? config;
+        static void Main()
+        {
+            BenchmarkRunner.Run<FileProcess>(new CustomConfig());            
+        }
+    }
+}
