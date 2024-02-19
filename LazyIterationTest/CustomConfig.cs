b656b6e22b010ba70b5dcc1883735ee714891b97
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-18 21:49:54 -0300

    feat: Add classe de configuração customizada para BenchmarkDotNet

---
+++ LazyIterationTest/CustomConfig.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+
+namespace LazyIterationTest
+{
+    public class CustomConfig : ManualConfig
+    {
+        public CustomConfig()
+        {
+            AddJob(Job.Default
+                .WithIterationCount(3)
+                .WithWarmupCount(1));
+
+            AddDiagnoser(MemoryDiagnoser.Default);
+            AddLogger(ConsoleLogger.Default);
+            AddColumnProvider(DefaultColumnProviders.Instance);
+        }
+    }
+}
